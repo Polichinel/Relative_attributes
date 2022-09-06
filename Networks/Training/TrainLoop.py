adf9b435058d741a7390ffc319bacc90373134ad
Polichinel <spm@ifs.ku.dk>
2022-09-06 20:44:22 +0200

    lr = 0.00005

--- Networks/Training/TrainLoop.py
+++ Networks/Training/TrainLoop.py
@@ -369,7 +369,7 @@ if __name__ == "__main__":
     hyperparameters = {
     "model_name" : model_name,
     "attribute" : attribute,
-    "learning_rate": 0.0001,
+    "learning_rate": 0.00005,
     "weight_decay" : 0.01,
     'betas' : (0.9, 0.999),
     "classes" : 1,
